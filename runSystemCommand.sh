913aecaf67ac2bef1ee2520b4f308e24a6b5a7ae
embedded_devops_sa <devops-team@hmcts.net>
2025-10-28 17:08:50 +0000

    Reverting the changes

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #F
 ##################################################################################################
 
-FRAMEWORK_VERSION=$(mvn help:evaluate -Dexpression=framework.version -q -DforceStdout)
+FRAMEWORK_VERSION=17.101.6
 CONTEXT_NAME="stagingdvla"
 USER_NAME="admin"
 PASSWORD="admin"
