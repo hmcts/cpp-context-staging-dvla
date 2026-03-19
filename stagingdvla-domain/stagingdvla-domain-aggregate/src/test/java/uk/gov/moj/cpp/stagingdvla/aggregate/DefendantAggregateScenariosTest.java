1526c80063240ff27f4c6b27b774166649cc62c2
yilmazsaritemur2 <150049030+yilmazsaritemur2@users.noreply.github.com>
2026-03-19 10:23:08 +0000

    disable test

--- stagingdvla-domain/stagingdvla-domain-aggregate/src/test/java/uk/gov/moj/cpp/stagingdvla/aggregate/DefendantAggregateScenariosTest.java
+++ stagingdvla-domain/stagingdvla-domain-aggregate/src/test/java/uk/gov/moj/cpp/stagingdvla/aggregate/DefendantAggregateScenariosTest.java
@@ -8,15 +8,32 @@ import uk.gov.moj.cpp.stagingdvla.aggregate.DefendantAggregateTestSteps.Scenario
 
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@Disabled
 class DefendantAggregateScenariosTest {
 
     public static Stream<Arguments> simpleScenario() {
-        return Stream.empty();
+        return Stream.of(
+                Arguments.of("DD-40319-ac1",
+                        defendantAggregateScenario()
+                                .withNotifyDriverStep(
+                                        "case hearing resulted",
+                                        "/testdata/drivernotifications/statdecapp/app-granted-offences-endorsed/ac1/case-resulted.json",
+                                        "/testdata/drivernotifications/statdecapp/app-granted-offences-endorsed/ac1/case-resulted-events-expected.json"
+                                )
+                                .withNotifyDriverStep(
+                                        "application hearing resulted",
+                                        "/testdata/drivernotifications/statdecapp/app-granted-offences-endorsed/ac1/application-hearing-resulted.json",
+                                        "/testdata/drivernotifications/statdecapp/app-granted-offences-endorsed/ac1/application-hearing-resulted-events-expected.json"
+                                )
 
+                )
+                // Additional scenarios can be added here
+        );
     }
 
     @ParameterizedTest(name = "{index} => {0}")
