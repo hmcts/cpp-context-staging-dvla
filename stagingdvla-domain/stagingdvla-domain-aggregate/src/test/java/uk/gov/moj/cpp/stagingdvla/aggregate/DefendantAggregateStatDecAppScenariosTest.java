db2c97745fd78e300b3ce6b2e38b7d700ac841ff
yilmazsaritemur2 <150049030+yilmazsaritemur2@users.noreply.github.com>
2026-05-18 16:32:02 +0100

    address comment

--- stagingdvla-domain/stagingdvla-domain-aggregate/src/test/java/uk/gov/moj/cpp/stagingdvla/aggregate/DefendantAggregateStatDecAppScenariosTest.java
+++ stagingdvla-domain/stagingdvla-domain-aggregate/src/test/java/uk/gov/moj/cpp/stagingdvla/aggregate/DefendantAggregateStatDecAppScenariosTest.java
@@ -207,30 +207,7 @@ class DefendantAggregateStatDecAppScenariosTest {
                                                 .add("previous", notNullValue()))
 
                 ),
-                Arguments.of("DD-40319-ac2B_1",
-                        defendantAggregateScenario()
-                                .withNotifyDriverStep(
-                                        "case hearing resulted",
-                                        "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/case-resulted.json",
-                                        "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/case-resulted-events.json",
-                                        jsonPathAssertions().add("notificationType", "New"))
-                                .withNotifyDriverStep(
-                                        "application hearing resulted",
-                                        "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/app-resulted.json",
-                                        "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/app-resulted-events.json",
-                                        jsonPathAssertions()
-                                                .add("notificationType", "Update")
-                                                .add("previous", notNullValue()))
-                                .withNotifyDriverStep(
-                                    "application hearing resulted",
-                                    "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/adj_app-resulted.json",
-                                    "/testdata/drivernotifications/statdec/dd-40319/ac2B_1/adj_app-resulted-events.json",
-                                jsonPathAssertions()
-                                        .add("notificationType", "Update")
-                                        .add("previous", notNullValue()))
-
-                ),
-                Arguments.of("DD-40319-ac2C",
+                Arguments.of("DD-40319-ac2C Both offences have endorsement results in the both application hearings",
                         defendantAggregateScenario()
                                 .withNotifyDriverStep(
                                         "case hearing resulted",
@@ -253,7 +230,8 @@ class DefendantAggregateStatDecAppScenariosTest {
                                                 .add("previous", notNullValue()))
 
                 ),
-                Arguments.of("DD-40319-ac2C1",
+                Arguments.of("DD-40319-ac2C1 fist offence resulted with endorsement and second offence is adjourn in the first " +
+                                "application hearing. In the adjourn hearing first offence doesn't have any result and second offence has endorsement result",
                         defendantAggregateScenario()
                                 .withNotifyDriverStep(
                                         "case hearing resulted",
@@ -271,6 +249,30 @@ class DefendantAggregateStatDecAppScenariosTest {
                                         "application hearing resulted",
                                         "/testdata/drivernotifications/statdec/dd-40319/ac2C1/app-second-resulted.json",
                                         "/testdata/drivernotifications/statdec/dd-40319/ac2C1/app-second-resulted-events.json",
+                                        jsonPathAssertions()
+                                                .add("notificationType", "Update")
+                                                .add("previous", notNullValue()))
+
+                ),
+                Arguments.of("DD-40319-ac2C2 fist offence resulted with endorsement and second offence is adjourn in the first " +
+                                "application hearing. In the adjourn hearing first offence has OATS result and second offence has endorsement result",
+                        defendantAggregateScenario()
+                                .withNotifyDriverStep(
+                                        "case hearing resulted",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/case-resulted.json",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/case-resulted-events.json",
+                                        jsonPathAssertions().add("notificationType", "New"))
+                                .withNotifyDriverStep(
+                                        "application hearing resulted",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/app-resulted.json",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/app-resulted-events.json",
+                                        jsonPathAssertions()
+                                                .add("notificationType", "Update")
+                                                .add("previous", notNullValue()))
+                                .withNotifyDriverStep(
+                                        "application hearing resulted",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/app-second-resulted.json",
+                                        "/testdata/drivernotifications/statdec/dd-40319/ac2C2/app-second-resulted-events.json",
                                         jsonPathAssertions()
                                                 .add("notificationType", "Update")
                                                 .add("previous", notNullValue()))
